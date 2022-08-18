6a867b552948306b4a8fe474523ce553d1152c74
Gnak <523748995@qq.com>
2022-08-18 18:56:17 +0800

    ipc通信调试

--- electron/main/ipcMainGetter.ts
+++ electron/main/ipcMainGetter.ts
@@ -1,12 +1,59 @@
 import { ipcMain } from "electron";
-import { cwd } from 'process'
+import { cwd } from "process";
+import { exec, spawn } from "child_process";
+// demo 获取pwd handle 的回调 Promise
+ipcMain.handle("getPwd", async (event, arg) => {
+  console.log(`[render]:${cwd()}`);
+  return cwd();
+});
 
-ipcMain.handle('getPwd', async (event, arg) => {
-    console.log(`[render]:${cwd()}`);
-    return cwd()
-})
+// ipcMain.on('runLocalProject', async (event, cmd) => {
+//     console.log(`[render]:${cmd}`);
+//     exec(cmd, {
+//         encoding: 'utf8',
+//     }, (err, stdout, stderr) => {
+//         if(err) {
+//             throw err
+//         }
 
-ipcMain.on('runLocalProject', async (event, ...args) => {
-    console.log(`[render]:${cwd()}`);
-    return cwd()
-})
+//         console.log(`render-run-stdout${stdout}`);
+//         console.log(`render-run-stderr${stderr}`);
+//     })
+// })
+
+// path: 运行命令的pwd
+// cmd 需要运行的命令
+ipcMain.handle("runLocalProject", async (event, path, cmd) => {
+  console.log(`[render] => path: ${path}  === cmd: ${cmd}`);
+  await new Promise((resolve, reject) => {
+    const _cmd = spawn(cmd, [], {
+      cwd: path,
+      shell: true,
+    });
+
+    _cmd.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    _cmd.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    _cmd.on("close", (code) => {
+      resolve(1);
+      console.log(`child process exited with code ${code}`);
+    });
+
+  });
+  return 'success'
+  // exec(cmd, {
+  //     encoding: 'utf8',
+  // }, (err, stdout, stderr) => {
+  //     if(err) {
+  //         throw err
+  //     }
+
+  //     console.log(`render-run-stdout${stdout}`);
+  //     console.log(`render-run-stderr${stderr}`);
+  // })
+});
--- src/views/ebidsun/index.vue
+++ src/views/ebidsun/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
         标信
-        <button @click="checkPwd">当前项目路径</button>
+        <button @click="checkPwd">获取当前项目路径</button>
         <p>当前目录：{{ pwdPath }}</p>
+        <button @click="runPyJenkinsProject"> 运行指定项目 </button>
+        <button @click="runBuildCmd"> 测试运行ebidsun-web-devtool打包命令 {{ isRunning }}</button>
   </div>
 </template>
 <script lang="ts" setup>
@@ -17,6 +19,19 @@ import { ipcRenderer  } from 'electron';
         console.log('[主进程发送来的]： ' + pwd);
         pwdPath.value = pwd
     }
+    const isRunning = ref<boolean>(false)
+    const runPyJenkinsProject = () => {
+        ipcRenderer.send('runLocalProject', `cd E:\\project\\ebidsun-jenksin-server && python main.py`)
+    }
+
+    const runBuildCmd = async () => {
+        isRunning.value = true;
+        console.log('执行中。。。')
+        await ipcRenderer.invoke('runLocalProject', `E:\\project\\ebidsun-web-devtool`, `npm run build`)
+        isRunning.value = false
+        console.log("执行完毕。。。")
+    }
+
 </script>
 <style lang='less' scoped>
 </style>    
